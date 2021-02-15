206b148432893be5267bb516ce200fd36a0f7e82
Thiago Cariuska <tcariuska@gmail.com>
2021-02-15 19:11:53 -0300

    hello

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+app.get('/hello', (req, res) => {
+    res.send('Hello User');
+});
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('Listening'))
